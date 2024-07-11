24ce231ebe08a5be0253ebc169005e94e6a3449d
JuanCE7 <juancastiestrella@gmail.com>
2024-07-11 10:27:56 -0500

    --

--- eslint.config.js
+++ eslint.config.js
@@ -1,35 +1,14 @@
 module.exports = {
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-      ecmaVersion: 2021,
-      sourceType: 'module',
-    },
     env: {
       browser: true,
       es2021: true,
       node: true,
     },
-    plugins: ['@typescript-eslint', 'react'],
     extends: [
       'eslint:recommended',
-      'plugin:@typescript-eslint/recommended',
-      'plugin:react/recommended',
     ],
-    settings: {
-      react: {
-        version: 'detect',
-      },
-    },
     rules: {
-      // Add your rules here
+      // Aquí puedes agregar tus reglas personalizadas
     },
-    overrides: [
-      {
-        files: ['*.ts', '*.tsx'],
-        rules: {
-          // TypeScript-specific rules (if any)
-        },
-      },
-    ],
   };
   
