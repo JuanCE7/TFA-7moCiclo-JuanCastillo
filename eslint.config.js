f16d9693b0c18f2b369adcf9978a52f65b69d054
JuanCE7 <juancastiestrella@gmail.com>
2024-07-11 10:09:52 -0500

    otro

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,19 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-
-
-export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-];
+module.exports = {
+  // Configuración global de ESLint
+  globals: {
+    // Configuración de variables globales específicas, como globals.browser
+    // Asegúrate de definir globals.browser correctamente
+    globals: 'globals.browser'
+  },
+  // Configuraciones recomendadas del plugin JS de ESLint
+  extends: [
+    'plugin:js/recommended'
+    // Otras extensiones recomendadas si las necesitas
+  ],
+  // Reglas adicionales o personalizadas
+  rules: {
+    // Aquí puedes agregar reglas adicionales o personalizadas
+    // Ejemplo:
+    // 'constructor-super': 'error',
+  },
+};
